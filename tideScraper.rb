dad66368ce644637803176fa0674ff218875d733
Jimmy Kuruvilla <jimmyjk@gmail.com>
2015-02-23 09:20:43 -0500

    accepts years and stations numbers correctly

--- tideScraper.rb
+++ tideScraper.rb
@@ -4,29 +4,29 @@ require 'net/http'
 require 'json'
 
 class TideScraper
-	attr_accessor :product, :application, :station, :datum, :units, :time_zone, :format, :response, :station_list, :years, :begin_month, :begin_day, :end_month, :end_day, :begin_date, :end_date, :query_params
+	attr_accessor :product, :application, :station, :datum, :units, :time_zone, :format, :response, :station_list, :years, :begin_year, :begin_month, :begin_day, :end_year, :end_month, :end_day, :begin_date, :end_date, :query_params
 
 	def initialize
 		@host_path={host: "tidesandcurrents.noaa.gov", path: "/api/datagetter"}
-		@product = "hourly_height"
+
 		@application = "NOS.COOPS.TAC.WL"
-		@station = "8735180"
+		@format = "csv"
+		@station = ""
+		@begin_year = ""
+		@end_year = ""
 
-		@begin_year ="2013"
-		@begin_month = "02"
-		@begin_day = "17"
+		@begin_month = "01"
+		@begin_day = "01"
+		@end_month = "12"
+		@end_day = "31"
 
-		@end_year ="2014"
-		@end_month = "02"
-		@end_day = "17"
-
-		@years = [2011,2012,2013]
 		@datum = "MLLW"
 		@units = "english"
 		@time_zone = "GMT"
-		@format = "csv"
-		
+		@product = "hourly_height"
 		@station_list = ["8735180", "8531680", "8519483"] ##fill in this array with station ids as Strings
+		# @station_list = ["8420411"]
+		@years = [2011,2012,2013,2014] ## enter years of data required
 
 	end
 
@@ -34,33 +34,36 @@ class TideScraper
 		return {product: "#{@product}", application: "#{@application}", station: "#{@station}", begin_date: "#{@begin_year}#{@begin_month}#{@begin_day}", end_date: "#{@end_year}#{@end_month}#{@end_day}",  datum: "#{@datum}", units: "#{@units}", time_zone: "#{@time_zone}", format: "#{@format}"}
 	end
 
-	def scrape
-		uri=URI::HTTP.build(@host_path)
-		uri.query=URI.encode_www_form(query_params)
-		@response=Net::HTTP.get(URI(uri.to_s))
-	end	
-
 	def writeFile
-		# f = File.open("#{@station}.#{@begin_date}-#{@end_date}.csv", "w")
 		f = File.open("#{@station}.csv", "a")
 		f.write(@response)
 		f.close
 	end
 
+	def scrape
+		uri=URI::HTTP.build(@host_path)
+		uri.query=URI.encode_www_form(query_params)
+		puts query_params
+		@response=Net::HTTP.get(URI(uri.to_s))
+	end	
+
+	def scrape_all
+		self.station_list.each do |station_item|
+			self.station = station_item
+			i=0
+			while i <= self.years.length-1 do 
+				self.begin_year = "#{self.years[i]}"
+				self.end_year = "#{self.years[i]}"
+				self.scrape
+				self.writeFile
+				i += 1
+			end
+		end
+
+	end
+
+
 end
 
 t = TideScraper.new
-
-t.station_list.each do |station|
-	t.station = station
-	i=0
-	while i < t.years.length-1 do 
-		t.begin_date = "#{t.years[i]}#{t.begin_month}#{t.begin_day}"
-		t.end_date = "#{t.years[i+1]}#{t.end_month}#{t.end_day}"
-		t.scrape
-		t.writeFile
-		i += 1
-	end
-end
-
-# needs trouble shooting but should be working. 
+t.scrape_all
